68496565212cef1fccedc73dfe2a0e3b1d59da3f
Zachary McCormick <zmccormick6@gmail.com>
2017-04-08 23:48:41 -0500

    Add Keyvi changes and Movement

--- Key Game/Assets/Scripts/TextController.cs
+++ Key Game/Assets/Scripts/TextController.cs
@@ -10,23 +10,36 @@ public class TextController : MonoBehaviour
     [SerializeField] private GameObject NextButton;
     [SerializeField] private GameObject EndButton;
     [SerializeField] private GameObject Keyvi;
+    [SerializeField] private GameObject InGameKeyvi;
+
+    Vector2 KeyviPosition, WhereToGo;
     private Animator KeyviAnim;
+    bool move = false;
+    float startTime;
 
     string[] message = 
         {"Hiya!  My name's Keyvi!",
-         "Do you just no talk or...ok?",
+         "Do you just not talk or...?",
          "Oh man, I'm blushing so the Dev's can show off the sprite.",
          "How rude of them.",
          "I don't know you, get away from me.",
-         "Dont' make me call the dungeon Police.",
+         "Don't make me call the Dungeon Police.",
          "Some say that there's a secret at the end of this dungeon.",
          "Totally not a boss that's missing or anything."};
-    int[] emotion = {0, 0, 1, 1, 2, 2, 3, 3};
+    int[] emotion = {0, 0, 1, 1, 2, 3, 2, 3};
     int counter = 0;
 
     void Start()
     {
         KeyviAnim = Keyvi.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (move == true)
+        {
+            InGameKeyvi.transform.position = Vector2.Lerp(KeyviPosition, WhereToGo, ((Time.time - startTime) / 4));
+        }
     }
 
     public void StartTalking()
@@ -38,6 +51,8 @@ public class TextController : MonoBehaviour
 
     private IEnumerator TypingOverTime(string[] message, int counter)
     {
+        InGameKeyvi = GameObject.Find("Keyvi");
+
         KeyviAnim.SetInteger("Emotion", emotion[counter]);
 
         NextButton.SetActive(false);
@@ -66,8 +81,21 @@ public class TextController : MonoBehaviour
             EndButton.SetActive(true);
     }
 
+    private IEnumerator MoveKeyvi()
+    {
+        yield return new WaitForSeconds(3f);
+        startTime = Time.time;
+        move = true;
+
+        KeyviPosition = new Vector2(InGameKeyvi.transform.position.x, InGameKeyvi.transform.position.y);
+        WhereToGo = new Vector2(InGameKeyvi.transform.position.x, 20);
+        yield return new WaitForSeconds(2f);
+        move = false;
+    }
+
     public void EndTalking()
     {
+        StartCoroutine(MoveKeyvi());
         MessageHolder.SetActive(false);
         DisplayMessage.text = null;
         GameObject.Find("Game Manager").GetComponent<LevelSwitch>().pause = false;
